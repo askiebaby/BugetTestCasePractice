ee3edd3d5f073d343605356d9ac50d0ed1bd9722
Raymond Liu (RD-TW) <raymond_liu@trend.com.tw>
2020-02-20 17:17:27 +0800

    Refactor test

--- test_budget_manager.py
+++ test_budget_manager.py
@@ -33,94 +33,38 @@ class TestBugetQuery(unittest.TestCase):
     def test_non_exist(self):
         self.assertEqual(self.b.query_budget('20190801', '20191031'), 0)
 
-
-class TestBugetQuery2(unittest.TestCase):
-    def setUp(self):
-        self.b = BugetManager('test_query2.db')
-        self.b.create_budget('202002', 29000)
-
-    def tearDown(self):
-        os.remove('test_query2.db')
-
-    def test_query_full_month(self):
-        self.assertEqual(self.b.query_budget('20200201', '20200229'), 29000)
-
-
-class TestBugetQuery3(unittest.TestCase):
-    def setUp(self):
-        self.b = BugetManager('test_query3.db')
-        self.b.create_budget('202002', 29000)
-        self.b.create_budget('202003', 31000)
-
-    def tearDown(self):
-        os.remove('test_query3.db')
-
-    def test_query_full_2_month(self):
-        self.assertEqual(self.b.query_budget('20200201', '20200331'), 60000)
-
-
-class TestBugetQuery4(unittest.TestCase):
-    def setUp(self):
-        self.b = BugetManager('test_query4.db')
-        self.b.create_budget('202003', 31000)
-
-    def tearDown(self):
-        os.remove('test_query4.db')
-
-    def test_query_one_day(self):
-        self.assertEqual(self.b.query_budget('20200301', '20200301'), 1000)
-
-
-class TestBugetQuery5(unittest.TestCase):
-    def setUp(self):
-        self.b = BugetManager('test_query5.db')
+    def test_query_not_exist_2(self):
+        self.b.create_budget('201912', 62000)
         self.b.create_budget('202001', 31000)
         self.b.create_budget('202002', 29)
+        self.assertEqual(self.b.query_budget('20000101', '20000102'), 0)
 
-    def tearDown(self):
-        os.remove('test_query5.db')
+    def test_query_full_1_month(self):
+        self.b.create_budget('202002', 29000)
+        self.assertEqual(self.b.query_budget('20200201', '20200229'), 29000)
+
+    def test_query_full_2_month(self):
+        self.b.create_budget('202002', 29000)
+        self.b.create_budget('202003', 31000)
+        self.assertEqual(self.b.query_budget('20200201', '20200331'), 60000)
+
+    def test_query_one_day(self):
+        self.b.create_budget('202003', 31000)
+        self.assertEqual(self.b.query_budget('20200301', '20200301'), 1000)
 
     def test_query_partial_cross_month(self):
+        self.b.create_budget('202001', 31000)
+        self.b.create_budget('202002', 29)
         self.assertEqual(self.b.query_budget('20200131', '20200202'), 1002)
 
-
-class TestBugetQuery6(unittest.TestCase):
-    def setUp(self):
-        self.b = BugetManager('test_query6.db')
+    def test_query_partial_cross_3_month(self):
         self.b.create_budget('202001', 31000)
         self.b.create_budget('202002', 29)
         self.b.create_budget('202003', 62000)
-
-    def tearDown(self):
-        os.remove('test_query6.db')
-
-    def test_query_partial_cross_3_month(self):
         self.assertEqual(self.b.query_budget('20200131', '20200301'), 3029)
 
-
-class TestBugetQuery7(unittest.TestCase):
-    def setUp(self):
-        self.b = BugetManager('test_query7.db')
-        self.b.create_budget('201912', 62000)
-        self.b.create_budget('202001', 31000)
-        self.b.create_budget('202002', 29)
-
-    def tearDown(self):
-        os.remove('test_query7.db')
-
     def test_query_partial_cross_year(self):
-        self.assertEqual(self.b.query_budget('20191231', '20200101'), 3000)
-
-
-class TestBugetQuery8(unittest.TestCase):
-    def setUp(self):
-        self.b = BugetManager('test_query8.db')
         self.b.create_budget('201912', 62000)
         self.b.create_budget('202001', 31000)
         self.b.create_budget('202002', 29)
-
-    def tearDown(self):
-        os.remove('test_query8.db')
-
-    def test_query_not_exist(self):
-        self.assertEqual(self.b.query_budget('20000101', '20000102'), 0)
+        self.assertEqual(self.b.query_budget('20191231', '20200101'), 3000)
